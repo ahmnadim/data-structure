727ae6f186a3b339fc468bed504703d7c32a9d76
ahmnadim <ahmnadim.2018@gmail.com>
2022-01-31 23:05:26 +0600

    singlyLinkedList pop

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -8,7 +8,7 @@ class Node{
 class SinglyLinkedList{
     constructor(){
         this.head = null;
-        this.tale = null;
+        this.tail = null;
         this.length = 0;
     }
 
@@ -16,16 +16,32 @@ class SinglyLinkedList{
         var newNode = new Node(val)
         if(!this.head){
             this.head = newNode
-            this.tale = newNode
+            this.tail = newNode
         }else{
-            this.tale.next = newNode
-            this.tale = newNode
+            this.tail.next = newNode
+            this.tail = newNode
 
         
         }
         this.length++
         return this
-        
-        
+    }
+
+    pop(){
+        if(!this.head) return undefined;
+
+        var current = this.head;
+        var newTail = current;
+
+        while(current.next){
+            newTail = current;
+            current = current.next;
+
+        }
+
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        return current;
     }
 }
