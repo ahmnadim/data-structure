54b074f67feb8a1c58915332c0152a17a4ebe595
ahmnadim <ahmnadim.2018@gmail.com>
2022-02-02 15:02:52 +0600

    get

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -72,4 +72,17 @@ class SinglyLinkedList{
         this.length++;
         return this.head;
     }
+
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+
+        var counter = 0;
+        var current = this.head;
+        while(counter !== index){
+            current = current.next;
+            counter++
+        }
+
+        return current;
+    }
 }
