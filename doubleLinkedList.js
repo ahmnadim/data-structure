6d4be1faa990854afe50bb67502cddc77e45f8b6
ahmnadim <ahmnadim.2018@gmail.com>
2022-02-19 00:23:32 +0600

    dll done

--- doubleLinkedList.js
+++ doubleLinkedList.js
@@ -63,6 +63,85 @@ class DoublyLinkList{
         this.length--;
         return this.shiftNode;
     }
+
+    unshift(val){
+        var newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        var current = this.head;
+        if(index <= this.length/2){
+            var count = 0;
+            while(index !== count){
+                current = current.next;
+                count++;
+            }
+        }else{
+            var count = this.length-1;
+            while(index !== count){
+                current = current.next;
+                count--;
+            }
+        }
+        return current;
+        
+    }
+
+    set(index, val){
+        var item = this.get(index);
+        if(!item)return false;
+        item.val = val;
+        return true;
+    }
+
+    insert(index, val){
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return !!this.unshift(val);
+        if(index === this.length) return !!this.push(val);
+
+        var newNode = new Node(val);
+        var beforeNode = this.get(index-1);
+        var afterNode = beforeNode.next;
+
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        afterNode.prev = newNode;
+        newNode.next = afterNode;
+        this.length++;
+
+        return true;
+        
+    }
+
+    remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length-1) return this.pop();
+
+        var removeNode = this.get(index);
+        var beforeNode = removeNode.prev;
+        var afterNode = removeNode.next;
+
+        beforeNode.next = afterNode;
+        afterNode.prev = beforeNode;
+        removeNode.next = null;
+        removeNode.prev = null;
+
+        this.length--;
+        return removeNode;
+    }
 }
 
 var dll = new DoublyLinkList()
